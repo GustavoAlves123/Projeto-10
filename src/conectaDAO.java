cfa5aefb2bb8a88206d34a24701c0524c44f5abe
Gustavo Henrique <gustavohss1010@gmail.com>
2024-05-26 17:14:58 -0300

    Botao de salvar

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -22,10 +22,10 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11", "root" ,"Mp260103$");
+            JOptionPane.showMessageDialog(null, "Cadastrado com sucesso");
         } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+            JOptionPane.showMessageDialog(null, "Erro ConectaDAO " + erro.getMessage());
         }
         return conn;
     }
